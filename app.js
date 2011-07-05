25547851c20cf86c8a7c5f0df54d6ac7d44af8c3
unknown <Admissions@.(none)>
2011-07-05 16:56:51 -0400

    Added Collection classes for officers and states.

--- app.js
+++ app.js
@@ -9,10 +9,6 @@
   	};
   };
   
-  officerCollection = function(){
-  	
-  };
-  
   usMapView = function(){
   	this.defaultColor = "#d3d3d3";
   	this.highlightColor = "#777777";
@@ -67,3 +63,55 @@
   		
   	};
   };
+  
+officerModel = function(fullname, title, email, states, counties, territories, qna){
+	this.id = fullname.replace(" ", "_");
+	
+	this.title = title;
+	
+	this.email = email;
+	
+	this.states = states;
+	
+	this.counties = counties;
+	
+	this.territories = territories;
+	
+	this.qna = qna;
+}
+
+stateModel = function(id, name, counties){
+	this.id = id;
+	
+	this.name = name;
+	
+	this.counties = counties;
+}
+
+collection = function(model){
+	this.model = model;
+	this.add = function(obj){
+		this.data.push( new this.model(obj) );
+	};
+	this.data = [];
+}
+
+officerCollection = new collection(officerModel);
+
+officerCollection.lookup = {};
+
+officerCollection.add = function(fullname, title, email, states, counties, territories, qna){
+	var pointer = officerCollection.data.push( new officerCollection.model( fullname, title, email, states, counties, territories, qna ) );
+	officerCollection.data[ pointer ].reference = officerCollection.data[ pointer ];
+	officerCollection.lookup[ officerCollection.data[ pointer ].id ] = officerCollection.data[ pointer ];
+}
+
+stateCollection = new collection(stateModel);
+
+stateCollection.lookup = {};
+
+stateCollection.add = function( id, name, counties ){
+	var pointer = stateCollection.data.push( new stateCollection.model(  id, name, counties ) );
+	stateCollection.data[ pointer ].reference = stateCollection.data[ pointer ];
+	stateCollection.lookup[ stateCollection.data[ pointer ].id ] = stateCollection.data[ pointer ];
+}
