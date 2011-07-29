7f77e04104e7409a2ca9061b9750a1446899adb3
unknown <Admissions@.(none)>
2011-07-29 17:03:37 -0400

    added state vector file.

---
+++ js/officers.js
@@ -0,0 +1,301 @@
+var officerCollection = [
+	{
+		fullname : "Paul Kwant",
+		title : "SENIOR RECRUITING OFFICER",
+		email : "kwantp@ferris.edu",
+		states : [ "me", "nh", "vt", "ma", "ct", "md", "pa", "ri", "ny", "nj",
+				"de" ],
+		counties : [ "Arenac", "Isabella", "Midland", "Bay", "Huron",
+				"Gratiot", "Saginaw", "Tuscola", "Sanilac", "Clinton",
+				"Shiawassee", "Genesee", "Lapeer", "St. Clair" ],
+		territory : "Gladwin, Midland, Bay, Huron, Saginaw, Sanilac, Gratiot, Tuscola, Arenac, Clare, Isabella, Montcalm & the states of Kentucky, Tennessee, New Yor",
+		qna : [
+				{
+					question : "Why did you pick Ferris?",
+					answer : "I selected Ferris as my Alma matter because of the practical educational opportunities the school provides. Ferris State University offers many innovative majors, small classes and hands-on teaching. I am proud that we are focused on assisting students with their career goals. Ferris State University has played an important role in my family history. My father worked here for 30 years before retiring. My wife, brother, sisters, and sister-in-law have all attended Ferris as students as well"
+				},
+				{
+					question : "What makes Ferris standout over other schools?",
+					answer : "There are many activities that students can get involved in while on-campus. One worth checking out is our Student Ambassador Organization. Our Student Ambassadors are an integral part of our admissions team and have a lot of fun while assisting our campus visitors!"
+				},
+				{
+					question : "What advice would you give to an incoming Freshman/Transfer Student?",
+					answer : "I recommend that you get involved and make new friends while here on campus. Life is way too short to stay in your Residence Hall room by yourself. Hang out at Starbucks, or attend a Hockey game. Your time here on campus is what you make of it! Get out there! One of my favorite things to do on campus is participate in the Homecoming game tailgate festivities. The College of Engineering Technology Chili Cook-off rocks!"
+				},
+				{
+					question : "When you are not recruiting students what do you do in your past time?",
+					answer : "When not recruiting students, I enjoy spending time with my family, watching movies, Detroit Red Wing hockey, and playing my guitars."
+				} ]
+	},
+	{
+		fullname : "Krista Hansen Elkins",
+		title : "Recruiter / Financial Aid Officer",
+		email : "hansenk8@ferris.edu",
+		states : [ "ind", "wi", "il" ],
+		counties : [ "Allegan", "St. Joseph", "Kalamazoo", "Berrien", "Cass",
+				"Van Buren", "Calhoun", "Branch", "Barry" ],
+		territory : "Allegan, St. Joseph, Kalamazoo, Berrien, Cass, Van Buren & the state of Illinois",
+		bio : "Hello, my name is Krista Hansen originally from Battle Creek, I am now happy to live in St. Joseph near Lake Michigan's beautiful beaches. I earned my Bachelor of Music Education degree in 2003 and spent four years teaching instrumental music in the public schools.<br />Please don't hesitate to contact me with any questions regarding the admission or financial aid processes. If you\'d like to set up a meeting with me, my office is conveniently located at our branch campus in Dowagiac on the campus of Southwestern Michigan College.",
+		qna : [
+				{
+					question : "What is the one item you would encourage students to check out at Ferris before graduating?",
+					answer : "Take a serious look at our study away programs. You have a chance to spend a semester or a year in another country while earning credits toward your degree. It is surprisingly affordable and a life changing experience!"
+				},
+				{
+					question : "When you are not recruiting students what do you do in your past time?",
+					answer : "I enjoy music, writing, reading, and spending time with my friends and family."
+				},
+				{
+					question : "What advice would you give to an incoming Freshman/Transfer Student?",
+					answer : "Get involved with activities on campus. Whether you're into sports or music, politics or Greek life, there really is something for everyone here. You will meet people and enjoy your college experience so much more if you get involved in activities that interest you."
+				},
+				{
+					question : "Favorite thing to do in Big Rapids?",
+					answer : "Canoeing or floating in a tube down the Muskegon River with a group of friends."
+				},
+				{
+					question : "Favorite Spot on Campus?",
+					answer : "My favorite spot on campus is the Rock Dining Center. I have eaten at many different campus dining facilities and I will tell you that the food at Ferris is truly exceptional.  You won't go hungry!"
+				} ]
+	},
+	{
+		fullname : "Amber Balmer",
+		title : "Recruiting Officer",
+		email : "balmera@ferris.edu",
+		states : [ "mo" ],
+		counties : [ "Kent", "Mecosta" ],
+		territory : "Kent County & The state of Missouri.",
+		qna : [
+				{
+					question : "What is your favorite tradition at Ferris?",
+					answer : "My favorite tradition at Ferris is Homecoming. It's the best time of year and I get to see all my long lost friends."
+				},
+				{
+					question : "What makes Ferris standout over other schools?",
+					answer : "Ferris is a fun loving community that works together. This school provides unique opportunities to all students and promotes individual growth. Ferris stands out above the rest because the amazing student body, faculty, and administration."
+				},
+				{
+					question : "What is the one item you would encourage students to check out at Ferris before graduating?",
+					answer : "The one thing a student must check out before they graduate from Ferris State is a Student Government Association meeting! Student Government is dear to my heart and provided me an \"out of class room\" education."
+				},
+				{
+					question : "When you are not recruiting students what do you do in your past time?",
+					answer : "In my free time I like to brush up on new trends in social media, read read read, cook, garden, and try new things like yoga!"
+				},
+				{
+					question : "What advice you would give to an incoming Freshman/Transfer Student?",
+					answer : "My advice to an incoming student is take advantage of all the on campus activities and organizations. There is so much to do on campus... see the comedians, watch a movie in the quad, join a professional organization, be a student ambassador, eat pancakes until you fall over at Pancakes with the president, and have fun!!"
+				},
+				{
+					question : "Favorite thing to do in Big Rapids?",
+					answer : "My favorite thing to do in Big Rapids...that's a tie between eating lemon sorbet in a waffle cone at the Old Pioneer Store and Tubing down the river in the middle of July! (I just had a thought I should try both at the same time)"
+				},
+				{
+					question : "Favorite Spot on Campus?",
+					answer : "My favorite spot on campus is the clock tower at noon! It makes me proud to be a bulldog!"
+				} ]
+	},
+	{
+		fullname : "Natalia Carvalho-Pinto",
+		title : "Recruiting Officer",
+		email : "carv9@ferris.edu",
+		states : [ "wa", "or", "ca", "ak", "hi", "id", "nv", "ut", "az", "wy",
+				"co", "nm", "mt" ],
+		counties : [ "Oceana", "Newaygo", "Muskegon", "Ottawa" ],
+		territory : "Mecosta, Mason, Lake, Newaygo, Muskegon, Ottawa, Osceola, Oceana & the state of California",
+		qna : [
+				{
+					question : "Why did you pick Ferris?",
+					answer : "Originally from Sao-Paulo, Brazil, I first came to Ferris as an International Student to study English for one year, but I quickly realized that Ferris was a great fit for me.  I loved campus life and campus events, found my fit and made friends through the Student Organizations and Athletic Events, and learned that I could succeed academically here through all the resources available to students, such as our SLA program, tutoring center and career counseling.  I am happy to say now that Ferris has been my home for the last 8 years.  I love working with students and their families, and helping them take their first steps towards a college degree and their career goals."
+				},
+				{
+					question : "What is your favorite memory at Ferris?",
+					answer : "The Hockey games are great!"
+				},
+				{
+					question : "What is the one item you would encourage students to check out at Ferris before graduating?",
+					answer : "A hockey game. The Rock dining facility.  Homecoming week activities.  The International Festival of Cultures."
+				},
+				{
+					question : "What advice would you give to an incoming Freshman/Transfer Student?",
+					answer : "Get Involved.  Participate in as many student organizations and campus events as you are able to...  There are great things to do at Ferris.  Getting involved will make your college experience a lot more complete, more educational, fuller, and a lot more exciting!"
+				},
+				{
+					question : "What is your favorite thing to do in Big Rapids?",
+					answer : "Eating at Buffalo Wild Wings, tubing, and taking my daughter to the local parks."
+				},
+				{
+					question : "What is your favorite spot on campus?",
+					answer : "The Ferris Library for Information Technology and Education (FLITE).  It is one of the biggest, most complete and most beautiful libraries you will encounter in a higher education setting."
+				} ]
+	},
+	{
+		fullname : "Angela Garrey",
+		title : "Recruiter / Financial Aid Officer",
+		email : "garreya@ferris.edu",
+		states : [],
+		counties : [ "Keweenaw", "Houghton", "Ontonagon", "Gogebic", "Baraga",
+				"Iron", "Marquette", "Dickinson", "Menominee", "Alger",
+				"Delta", "Schoolcraft", "Luce", "Mackinaw", "Chippewa",
+				"Emmet", "Cheboygan", "Presque Isle", "Charlevoix", "Leelanau",
+				"Antrim", "Otsego", "Montmorency", "Alpena", "Benzie",
+				"Grand Traverse", "Kalkaska", "Crawford", "Oscoda", "Alcona",
+				"Manistee", "Wexford", "Missaukee", "Roscommon", "Ogemaw",
+				"Iosco", "Mason", "Lake", "Osceola", "Clare", "Gladwin", ],
+		territory : "Alcona, Alger, Alpena, Baraga, Cheboygan, Chippewa, Crawford, Delta, Dickinson, Gogebic, Houghton, Iosco, Iron, Keweenaw, Luce, Mackinac, Marquette, Menominee, Montmorency, Ogemaw, Ontonagon, Oscoda, Otsego, Presque Isle, Roscommon, Schoolcraft, Antrim, Benzie, Charlevoix, Emmet, Grand Traverse, Kalkaska, Leelanau, Manistee, Missaukee, Wexford & the state of Pennsylvania",
+		qna : [
+				{
+					question : "Why Did You Pick Ferris?",
+					answer : "I picked Ferris for a numerous reasons, but the number one reason it is fit me! The mid size campus, the faculty and the community of Ferris made me feel at like I was coming home."
+				},
+				{
+					question : "What is your favorite memory of Ferris?",
+					answer : "My favorite memory of Ferris would probably be when I met my now husband (we have the same birthday)."
+				},
+				{
+					question : "What is your favorite tradition of Ferris?",
+					answer : "My favorite tradition at Ferris would have to be all sorority rush. Watching young ladies open their bid cards is a tradition like no other it gives you a sense of pride of being in an organization with so many great young women."
+				},
+				{
+					question : "What makes Ferris standout over other schools?",
+					answer : "Ferris stands out compared to other schools with its peeps. I don't mean the candy, I mean all the amazing people who create the university. The students, the faculty, and the staff everyone you interact with on a day to day basis make you a better person and a better student."
+				},
+				{
+					question : "The one item you encourage student to check out at Ferris before graduating?",
+					answer : "The third floor of the Timme Center."
+				},
+				{
+					question : "Advice you would give to an incoming Freshman/Transfer Student?",
+					answer : "My advice to students coming in as a freshman or a transfer student is to use all the resources on campus available to you. Getting involved with the RSO's, going to sporting events, enjoying the restaurants of Big Rapids, knowing your faculty, taking a class SLA, find out more about the history of the institution, all of which are accessible to students."
+				},
+				{
+					question : "What is your favorite thing to do in Big Rapids?",
+					answer : "Going out to SCHUBERG'S with good friends, I have my own booth."
+				},
+				{
+					question : "What is your Favorite Spot on Campus?",
+					answer : "My favorite spot on campus would be the alumni building. It is where it all began and looking at the tile mosaic of the institutions true foundation is a great way to take pride in being a Ferris State Bulldog."
+				} ]
+	},
+	{
+		fullname : "Chris Higley",
+		title : "Admissions Officer",
+		email : "christine_higley@ferris.edu",
+		states : [ "nd", "sd", "ne", "ks", "ok", "tx", "mn", "ia", "ar", "la" ],
+		counties : [ "Montcalm", "Ionia", "Eaton", "Ingham", "Jackson",
+				"Hillsdale", "Lenawee" ],
+		territorys : "Barry, Eaton, Ingham, Calhoun, Jackson, Washtenaw, Branch, Hillsdale, Lenawee, Ionia, Clinton, Shiawassee & the state of Lowa",
+		qna : [
+				{
+					question : "&nbsp;",
+					answer : "Hello, my name is Chris Higley and I am your Admissions Officer for Ferris State University. I am very proud to say that I have not only been a student at Ferris, but I have worked for the University for 25 years."
+				},
+
+				{
+					question : "&nbsp;",
+					answer : "With so many years of experience in Higher Education I am confident that my strong ability to be a leader will be able to guide you through the process of selecting a college and applying for admissions and financial aid. As a member of the University, I am here to support you and your success as a student."
+				},
+
+				{
+					question : "What makes Ferris standout over other schools?",
+					answer : "The people here at the University are what makes Ferris really stand out from other Universities. Our faculty and staff really care about our students. We have a nice size campus with small classes which make it easy for the faculty to know you by name. We offer leadership opportunities, academic support and most of all we allow you to make it your own unique experience."
+				},
+
+				{
+					question : "What is your favorite tradition at Ferris?",
+					answer : "Every year I attend the most exciting Hockey game of the year, FSU vs. U of M. This event will be sure to turn you into a BULLDOG."
+				},
+
+				{
+					question : "What is the one item you would encourage students to check out at Ferris before graduating?",
+					answer : "I would encourage any student to get involved while attending Ferris. You can join one or more of the 210 student organizations or volunteer your time at the volunteer center. Attending the Job Fair is a must before your graduate. This is a great opportunity for you to find a job."
+				},
+
+				{
+					question : "What is your favorite spot on campus?",
+					answer : "My favorite part of the day or week is when I can stroll down the hall to Starbucks, grab a cup of coffee and interact with fellow staff members or students."
+				},
+
+				{
+					question : "",
+					answer : "I welcome you to contact me with any questions or concerns you may have about Ferris. The experience you will have at Ferris will provide you with the tools and knowledge that you will need to be successful."
+				} ]
+	},
+	{
+		fullname : "Tyrone Collins",
+		title : "Admissions Counselor",
+		email : "collinst@ferris.edu",
+		states : [ "oh" ],
+		counties : [ "Wayne", "Monroe" ],
+		territorys : "Macomb, Monroe, Wayne & the state of California",
+		qna : [
+				{
+					question : "",
+					answer : "WELCOME! I am your Admissions Counselor. I am originally from Monroe, LA, but have spent the majority of my life in southeast Michigan. I have a Bachelor of Science Degree in Public Administration from Ferris State University in 1977, Masters of Arts in Counseling - Higher Education, and a Master's of Science in Career and Technical Education."
+				},
+
+				{
+					question : "",
+					answer : "I currently advise the Wade McCree Incentive Scholarship, Compact Scholar students from the Detroit Public Schools and Ferris State University is a member of the YMCA College Coalition."
+				},
+
+				{
+					question : "What are your favorite memory and or tradition of Ferris?",
+					answer : "My favorite memory of going to Ferris State is hanging out on campus and constantly meeting new people. What stood out to me about Ferris State to me was how helpful the faculty was to me when I was serious about obtaining my education. I encourage you to check out the ROCK, the best food in town and my favorite campus spot, the LIBRARY."
+				},
+
+				{
+					question : "What advice would you give to an incoming Freshman/Transfer Student?",
+					answer : "My advice to any incoming student to Ferris State University is to come prepared as academically possible. If you feel stressed, Ferris State will assist you with your learning. Your aggressiveness toward your educational future will lead you to great rewards."
+				},
+
+				{
+					question : "",
+					answer : "Always remember that as an Admissions Counselor for Ferris State University,\" I WORK FOR YOU!\""
+				} ]
+	},
+	{
+		fullname : "Leah Goode",
+		title : "Recruiting Officer",
+		email : "goodel@ferris.edu",
+		states : [ "ky", "tn", "ms", "al", "ga", "fl", "sc", "nc", "va", "wv" ],
+		counties : [ "Macomb", "Washtenaw", "Oakland", "Livingston" ],
+		territorys : "Genesee, Lapeer, St. Clair, Oakland, Livingston & the state of Florida",
+		qna : [
+				{
+					question : "What makes Ferris standout over other schools?",
+					answer : "Ferris stands out over other schools because of its focus on the students and the way the University prepares its students for careers, not just jobs."
+				},
+
+				{
+					question : "What is the one item you would encourage students to check out at Ferris before graduating?",
+					answer : "Students should check out all of the student organizations on campus-find something that interests you and get involved!"
+				},
+
+				{
+					question : "When you are not recruiting students what do you do in your past time?",
+					answer : "My hobbies include reading, baking, cooking, and hiking. I also like to help out the FSU athletics department during volleyball and hockey games."
+				},
+
+				{
+					question : "What advice you would give to an incoming Freshman/Transfer Student?",
+					answer : "New FSU students need to attend all the orientation activities so they know about the great services and entertainment Ferris offers. Also, try and meet as many people as possible, and get out and try new things! College isn't just about learning in the classroom, a lot of learning takes place out of the classroom, too."
+				},
+
+				{
+					question : "What is your favorite thing to do in Big Rapids?",
+					answer : "Walking my dog at the Riverwalk."
+				},
+
+				{
+					question : "What is your favorite spot on campus?",
+					answer : "The Rock."
+				},
+
+				{
+					question : "Is there anything else we should know about you?",
+					answer : "I've worked at a college or university for the last seven years, so I know that choosing the right college and major is very important. Working in higher education is very rewarding and it's great to see how a college education can open doors for graduates."
+				}
+			]
+	}
+];
