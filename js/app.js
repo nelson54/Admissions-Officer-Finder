fbe5e20567a80ebdab077c3b9b28f81300259c29
Derek Nelson <dereknelson01@gmail.com>
2011-08-16 13:02:38 -0400

    Completed changes and pages.

--- js/app.js
+++ js/app.js
@@ -53,35 +53,53 @@ modal = function (id) {
 };
 
 // Application Implementation
-
-appController = function(map) {
+appController = function() {
 	
-    this.highlight = function (state) {
-        if (state !== "mi") {
-            var special = false
-            var states = officersCollection.stateLookup[state].states;
-        } else {
-            var special = true;
-            var states = [];
-        };
-
-        formView.highlightState(state);
-        map.highlight(state, states, special);
+	this.setMap = function(mapView){
+		if(this.currentMapView){
+			$('#no-menu-content div.map').empty();
+		}
+		this.currentMapView = window.currentMapView = mapView;
+		mapView.drawMap();
+		mapView.drillDownCallback();
+	}
+	
+    this.highlight = function (area, type) {
+    	if ( type === "state" ) {
+	        if (area !== "MI") {
+	            var special = false;
+	            var states = officersCollection.stateLookup[area].states;
+	        } else {
+	            var special = true;
+	            var states = [];
+	        };
+	        formView.highlight(area);
+        	window.currentMapView.highlight(area, states, special);
+	    } else {
+	    	var special = false;
+	    	var counties = officersCollection.countyLookup[area].counties;
+	    	formView.highlight(area);
+	    	window.currentMapView.highlight(area, counties, special);
+	    }
     };
 
-    this.choose = function (state) {
-        map.chooseState(state);
-        if (state !== "mi") {
-            new modal(officersCollection.stateLookup[state].id);
-        } else {
-            michiganModal();
-        };
+    this.choose = function (area, type) {
+        window.currentMapView.chooseArea(area);
+        if (type === "state"){
+	        if (area !== "MI") {
+	            new modal(officersCollection.stateLookup[area].id);
+	        } else {
+	            window.front.setMap(window.miMapView);
+	        };
+	    } else {
+	    	new modal(officersCollection.countyLookup[area].id);
+	    };
     };
 
     this.reset = function (formReset) {
     	if (!stopReset){
             formView.reset();
-            map.reset();
+            window.currentMapView.reset();
        }
     };
     
@@ -94,7 +112,6 @@ appController = function(map) {
 
 officerModel = function (fullname, title, email, states, counties, territories, qna, getStateName){
 
-	
 	this.getTerritories = function(counties, states){
     	if ( counties.length === 0 ){
     		var countyListStr = "";
@@ -144,84 +161,222 @@ stateModel = function (id, name, counties) {
     this.reference = undefined;
 }
 
-
-
 // View Interfaces
-usMapView = function () {
+UsMapView = function () {
 	this.self = this;
+	this.states = [];
     this.defaultColor = "#d3d3d3";
     this.highlightColor = "#777777";
     this.sectionHighlightColor = "#bbbbbb";
     this.specialHighlightColor = "#CC0033";
+    this.drillDownCallback = function(){window.formView = new drillDownFormView( false, "state", stateList )};
+	this.areaAttr = {
+	  "fill": "#d3d3d3",
+	  "stroke": "#fff",
+	  "stroke-opacity": "1",
+	  "stroke-linejoin": "round",
+	  "stroke-miterlimit": "4",
+	  "stroke-width": "0.75",
+	  "stroke-dasharray": "none"
+	};
+
+    this.setup = function (stte) {
+    	
+            stte.color = Raphael.getColor();
+            stte[0].style.cursor = "pointer";
+            stte[0].onmousedown = function () {
+                front.choose(this.raphael.name, "state");
+            };
+            stte[0].onmouseover = function () {
+                if (window.current != this.raphael.name && window.mapHover) {
+                    window.current = this.raphael.name;
+                    front.highlight(current, "state");
+                }
+            };
+            stte[0].onmouseout = function () {
+                window.current = null;
+                front.reset(true);
+            };
+            return stte;
+        }
+
+	this.drawMap = function(){
+		if(window.backButtonInstance){
+			window.backButtonInstance.destroy();
+		}
+		this.currentMapPaper = window.currentMapPaper = Raphael("united_states", 1000, 600);
+		for (var state in usMap) {
+	        var path = this.currentMapPaper.path(usMap[state]).attr(this.areaAttr);
+	        path.name = state.toUpperCase();
+	        window.usRaphael[state.toUpperCase()] = path;
+	        this.states.push(path);
+	    };
+	    this.states.map(this.setup);
+	};
 
     this.reset = function () {
-        for (var state in usRaphael) {
-            usRaphael[state].animate({
+        for (var state in window.usRaphael) {
+            window.usRaphael[state].animate({
                 fill: mapView.defaultColor
             }, 100);
         }
-        R.safari();
+        this.currentMapPaper.safari();
     };
 
-    this.chooseState = function (state) {
-        this.highlightState(state, false, this.specialHighlightColor);
+    this.chooseArea = function (area) {
+        this.highlightArea(area, false, this.specialHighlightColor);
     };
 
-    this.highlight = function (state, section, isSpecialArea) {
+    this.highlight = function (area, section, isSpecialArea) {
         if (isSpecialArea === true) {
             var color = this.specialHighlightColor
         } else {
             var color = this.highlightColor
         }
-        section.map(this.highlightState, this.self);
-        this.highlightState(state, false, color);
+        section.map(this.highlightArea, this.self);
+        this.highlightArea(area, false, color);
     };
 
-    this.highlightState = function (state, position, color) {
+    this.highlightArea = function (area, position, color) {
         if (typeof position === "number") {
             var color = this.sectionHighlightColor;
         }
-        usRaphael[state].animate({
+        window.usRaphael[area.toUpperCase()].animate({
             fill: color
         }, 100);
     };
 };
 
-function renderDrillDownForm(state) {
+MiMapView = function () {
+	this.self = this;
+	this.counties = [];
+    this.defaultColor = "#d3d3d3";
+    this.highlightColor = "#777777";
+    this.sectionHighlightColor = "#bbbbbb";
+    this.specialHighlightColor = "#CC0033";
+    
+    this.setupAreaList = function(area){
+    	return({abbr: area.replace(/. /g, "_").replace(/ /g, "_") , title : area});
+    }
+    
+    this.drillDownCallback = function(){window.formView = new drillDownFormView(false, "county", { options : stateCollection.lookup["MI"]["counties"].map(this.setupAreaList) } ) };
+	this.areaAttr = {
+	  "fill": "#d3d3d3",
+	  "stroke": "#fff",
+	  "stroke-opacity": "1",
+	  "stroke-linejoin": "round",
+	  "stroke-miterlimit": "4",
+	  "stroke-width": "0.75",
+	  "stroke-dasharray": "none"
+	};
+	
+    this.setup = function (cnty) {
+            cnty.color = Raphael.getColor();
+            cnty[0].style.cursor = "pointer";
+            cnty[0].onmousedown = function () {
+                front.choose(this.raphael.name, "county");
+            };
+            cnty[0].onmouseover = function () {
+                if (window.current != this.raphael.name && window.mapHover) {
+                    window.current = this.raphael.name;
+                    front.highlight(current, "county");
+                }
+            };
+            cnty[0].onmouseout = function () {
+                window.current = null;
+                front.reset(true);
+            };
+            return cnty;
+        }
+
+	this.drawMap = function(){
+		new miBackButton();
+		this.currentMapPaper = window.currentMapPaper = Raphael("michigan", 700, 700);
+		for (var cnty in miMap) {
+	        var path = this.currentMapPaper.path(miMap[cnty]).attr(this.areaAttr);
+	        path.name = cnty;
+	        window.miRaphael[cnty] = path;
+	        this.counties.push(path);
+	    };
+	    this.counties.map(this.setup);
+	};
+
+    this.reset = function () {
+        for (var cnty in miRaphael) {
+            miRaphael[cnty].animate({
+                fill: mapView.defaultColor
+            }, 100);
+        }
+        this.currentMapPaper.safari();
+    };
+
+    this.chooseArea = function (area) {
+        this.highlightArea(area, false, this.specialHighlightColor);
+    };
+
+    this.highlight = function (area, section, isSpecialArea) {
+        if (isSpecialArea === true) {
+            var color = this.specialHighlightColor
+        } else {
+            var color = this.highlightColor
+        }
+        section.map(this.highlightArea, this.self);
+        this.highlightArea(area, false, color);
+    };
+
+    this.highlightArea = function (area, position, color) {
+        if (typeof position === "number") {
+            var color = this.sectionHighlightColor;
+        }
+        window.miRaphael[area].animate({
+            fill: color
+        }, 100);
+    };
+};
+
+function miBackButton(){
+	this.render = function(){
+		window.backButtonInstance = this;
+	    var miBackSource = $("#mi-back-button").html();
+	    var miBackTemplate = Handlebars.compile(miBackSource);
+		var miBackHtml = miBackTemplate();
+		$("#head .nav").append(miBackHtml);
+	}
+	
+	this.destroy = function(){
+		window.backButtonInstance = false;
+		$("#head > .nav").html("");
+	}
+	
+	this.render();
+}
+
+function drillDownFormView(area, type, areaList) {
+	this.type = type;
     var drillDownSource = $("#drilldown-form-template").html();
     var drillDownTemplate = Handlebars.compile(drillDownSource);
-    var drillDownContext = stateList;
+    var drillDownContext = areaList;
 	var drillDownHtml = drillDownTemplate(drillDownContext);
     $("#drilldowndiv").remove();
     $("#no-menu-content").prepend(drillDownHtml);
 
-    if (state) {
-        $("form#drillDown select[name='states'] option[value='" + state + "']").attr("selected", "selected");
+    if (area) {
+        $("form#drillDown select option[value='" + area + "']").attr("selected", "selected");
     }
 
-    $("form#drillDown select[name='states']").change(function () {
-        var state = $("form#drillDown select[name='states']").val().toLowerCase();
-        renderDrillDownForm(state);
-        front.choose(state);
-    });
-}
-
-function drillDownFormView () {
-    this.highlightState = function (state) {
-        $('select[name="states"] option[value="' + state.toUpperCase() + '"]').attr("selected", "selected");
+    $("form#drillDown select").change( function () {
+        var selection = $("form#drillDown select").val();
+        front.choose(selection, type);
+    } );
+    
+    this.highlight = function (option) {
+        $('form#drillDown select option[value="' + option + '"]').attr("selected", "selected");
     };
 
     this.reset = function () {
-        $('select[name="states"] option[value="none"]').attr("selected", "selected");
+        $('form#drillDown select option[value="none"]').attr("selected", "selected");
     };
-
-    $('select[name="states"] option').mouseenter(function (e) {
-        front.highlight($(e.target).attr("value").toLowerCase());
-    })
-    $('select[name="states"] option').mouseout(function (e) {
-        front.reset(false);
-    })
-};
+}
 
 function michiganModal() {
     var drillDownSource = $("#modal-form-template").html();
@@ -246,7 +401,11 @@ function michiganModal() {
 // Collections
 // Define the officers collection.
 
-
+makeId = function(label){
+	idLabel = label.toString().replace(/. /g, "_");
+	idLabel = idLabel.replace(/ /g, "_");
+	return ( idLabel );
+}
 
 officersCollection = new collection(officerModel);
 officersCollection.lookup = {};
@@ -259,10 +418,10 @@ officersCollection.add = function (fullname, title, email, states, counties, ter
     this.self.data[pointer - 1].reference = this.self.data[pointer - 1];
     this.self.lookup[this.self.data[pointer - 1].id] = this.self.data[pointer - 1];
     for (var s in states) {
-        this.self.stateLookup[states[s]] = this.self.data[pointer - 1];
+        this.self.stateLookup[states[s].toString().toUpperCase()] = this.self.data[pointer - 1];
     }
     for (var c in counties) {
-        this.self.countyLookup[counties[c]] = this.self.data[pointer - 1];
+        this.self.countyLookup[makeId(counties[c])] = this.self.data[pointer - 1];
     }
 
     $('div#modals').append(this.self.template(this.self.data[pointer - 1]));
